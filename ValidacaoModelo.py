57dc49eb77e6bad4ee619fc8a3fc7b5fc53e3895
GUILHERME FABIO VIEIRA <41790294+guilhermefabio@users.noreply.github.com>
2023-05-23 21:26:53 -0300

    Update ValidacaoModelo.py

--- ValidacaoModelo.py
+++ ValidacaoModelo.py
@@ -1,217 +1,120 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import re
-
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import matplotlib.pyplot as plt
-
-import pandas as pd
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-
-
-
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 # Carregar o arquivo Excel em um dataframe
 df = pd.read_excel("areas_pesquisa_com_CAPES.xlsx")
 
-#df = pd.read_excel("novo_df_palavras_retiradas.xlsx")
-
-X = df["PALAVRAS_CHAVE"]
-
-print(X)
-y =df['Area_CAPES']
-print(y)
-# Criar o objeto CountVectorizer
-vectorizer = CountVectorizer()
-
-# Transformar as palavras-chave em um vetor de recursos
-X = vectorizer.fit_transform(X)
-
-# Criar o objeto MultinomialNB
-clf = MultinomialNB()
-
-# Treinar o classificador
-clf.fit(X, y)
-
-
-# Fazer predições com novos dados
-new_data = ['logistica, reciclagem, logistica reversa']
-new_data = vectorizer.transform(new_data)
-predictions = clf.predict(new_data)
-
-# Imprimir as predições
-print(predictions)
-
-#separando os dados em variáveis de treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
-
-#criando o modelo de Naive Bayes Multinomial
-model = MultinomialNB()
-model.fit(X_train, y_train)
-
-#realizando as previsões
-y_pred = model.predict(X_test)
-
-#calculando os indicadores de confiabilidade do modelo
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='macro')
-recall = recall_score(y_test, y_pred, average='macro')
-f1 = f1_score(y_test, y_pred, average='macro')
-
-#imprimindo os indicadores de confiabilidade do modelo
-print('Acurácia:', accuracy)
-print('Precisão:', precision)
-print('Recall:', recall)
-print('F1 Score:', f1)
-
-
-
-
-
-import pandas as pd
-import re
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Separar os dados em variáveis de treino e teste
-X_train, X_test, y_train, y_test = train_test_split(df["PALAVRAS_CHAVE"], df["Area_CAPES"], test_size=0.2, random_state=42)
-
-# Criar o objeto CountVectorizer
-vectorizer = CountVectorizer()
-
-# Ajustar o CountVectorizer aos dados de treinamento
-X_train = vectorizer.fit_transform(X_train)
-
-# Usar o CountVectorizer para transformar os dados de teste
-X_test = vectorizer.transform(X_test)
-
-# Criar o objeto MultinomialNB
-clf = MultinomialNB()
-
-# Treinar o classificador
-clf.fit(X_train, y_train)
-
-# Fazer predições com os dados de teste
-y_pred = clf.predict(X_test)
-
-# Calcular a matriz de confusão
-cm = confusion_matrix(y_test, y_pred)
-
-# Plotar a matriz de confusão em um heatmap
-sns.heatmap(cm, annot=True, cmap="Blues")
-plt.xlabel("Predito")
-plt.ylabel("Real")
-plt.show()
-
-
-# Calcular as palavras-chave mais ofensivas
-coef = clf.coef_
-keywords = vectorizer.get_feature_names()
-df_keywords = pd.DataFrame({"keywords": keywords, "coef": coef[0]})
-df_keywords = df_keywords.sort_values("coef", ascending=False).head(30)
-
-# Plotar um gráfico de barras das palavras-chave ofensivas
-plt.figure(figsize=(15,10))
-sns.barplot(data=df_keywords.head(30), x="keywords", y="coef")
-plt.xticks(rotation=90)
-plt.xlabel("Palavras-chave")
-plt.ylabel("Coeficiente")
-plt.title("Palavras-chave mais ofensivas")
-plt.show()
-
-# Remover as palavras-chave ofensivas do dataframe
-offensive_keywords = df_keywords["keywords"].tolist()
-df_cleaned = df[~df["PALAVRAS_CHAVE"].str.contains("|".join(offensive_keywords), case=False, regex=True)]
-
-# Salvar o dataframe limpo como um novo arquivo Excel
-df_cleaned.to_excel("novo_df_palavras_retiradas.xlsx", index=False)
-
-
-
-
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-
-# Carregar o arquivo Excel em um dataframe
-df = pd.read_excel("areas_pesquisa_com_CAPES.xlsx")
-
+# Separar as features (X) e o target (y)
 X = df["PALAVRAS_CHAVE"]
 y = df['Area_CAPES']
 
-# Criar o objeto CountVectorizer
+# Criar um vetorizador para converter as palavras-chave em vetores numéricos
 vectorizer = CountVectorizer()
+X_vectorized = vectorizer.fit_transform(X)
 
-# Transformar as palavras-chave em um vetor de recursos
-X = vectorizer.fit_transform(X)
+# Criar e treinar o modelo Naive Bayes com validação cruzada
+model = MultinomialNB()
+y_pred = cross_val_predict(model, X_vectorized, y, cv=5)
 
-# Criar o objeto MultinomialNB
-clf = MultinomialNB()
+# Calcular as diferenças entre as previsões e os valores reais
+differences = (y_pred != y)
 
-# Treinar o classificador
-clf.fit(X, y)
+# Obter os índices dos dados que causam sobreajuste
+overfitting_indices = np.where(differences)[0]
 
-# Fazer previsões com novos dados
-new_data = input("Insira os dados para previsão: ")
-new_data = vectorizer.transform([new_data])
-predictions = clf.predict(new_data)
+# Remover os dados que causam sobreajuste do dataframe original
+df_limpo = df.drop(overfitting_indices)
 
-# Imprimir as predições
-print(predictions)
+# Salvar o novo dataframe em um arquivo Excel
+df_limpo.to_excel("dados_limpos.xlsx", index=False)
+
+# Separar as features (X_limpo) e o target (y_limpo) dos dados limpos
+X_limpo = df_limpo["PALAVRAS_CHAVE"]
+y_limpo = df_limpo['Area_CAPES']
+
+# Criar um vetorizador para os dados limpos
+X_limpo_vectorized = vectorizer.transform(X_limpo)
+
+# Criar e treinar o modelo Naive Bayes com os dados limpos
+model_limpo = MultinomialNB()
+model_limpo.fit(X_limpo_vectorized, y_limpo)
+
+# Realizar previsões no conjunto de teste
+y_pred_limpo = model_limpo.predict(X_limpo_vectorized)
 
 
-'''
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Calcular as métricas para o modelo original
+accuracy_original = accuracy_score(y, y_pred)
+precision_original = precision_score(y, y_pred, average='weighted')
+recall_original = recall_score(y, y_pred, average='weighted')
+f1_score_original = f1_score(y, y_pred, average='weighted')
+confusion_mtx_original = confusion_matrix(y, y_pred)
 
-# Definir os modelos de machine learning a serem testados
-models = [
-    ('Logistic Regression', LogisticRegression(random_state=42)),
-    ('Decision Tree', DecisionTreeClassifier(random_state=42)),
-    ('Random Forest', RandomForestClassifier(random_state=42))
-]
+# Calcular as métricas para o modelo limpo
+accuracy_limpo = accuracy_score(y_limpo, y_pred_limpo)
+precision_limpo = precision_score(y_limpo, y_pred_limpo, average='weighted')
+recall_limpo = recall_score(y_limpo, y_pred_limpo, average='weighted')
+f1_score_limpo = f1_score(y_limpo, y_pred_limpo, average='weighted')
+confusion_mtx_limpo = confusion_matrix(y_limpo, y_pred_limpo)
 
-# Treinar os modelos com o conjunto de treinamento e fazer previsões com o conjunto de teste
-results = {}
-for name, model in models:
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    results[name] = {
-        'Accuracy': accuracy_score(y_test, y_pred),
-        'Precision': precision_score(y_test, y_pred),
-        'Recall': recall_score(y_test, y_pred),
-        'F1-score': f1_score(y_test, y_pred)
-    }
+# Configurar os nomes das classes para a matriz de confusão
+class_names = np.unique(np.concatenate((y, y_limpo)))
+# Configurar o tamanho da figura e das subplots
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 
-# Plotar os resultados em gráficos
-for metric in ['Accuracy', 'Precision', 'Recall', 'F1-score']:
-    plt.figure()
-    plt.bar(results.keys(), [result[metric] for result in results.values()])
-    plt.title(metric)
-    plt.show()
-'''
+# Plotar a matriz de confusão para o modelo original
+axs[0].imshow(confusion_mtx_original, interpolation='nearest', cmap=plt.cm.Blues)
+thresh_original = confusion_mtx_original.max() / 2.0
+for i in range(confusion_mtx_original.shape[0]):
+    for j in range(confusion_mtx_original.shape[1]):
+        axs[0].text(j, i, confusion_mtx_original[i, j],
+                    horizontalalignment="center",
+                    color="white" if confusion_mtx_original[i, j] > thresh_original else "black",
+                    fontsize=8)
+axs[0].set_title('Matriz de Confusão (Modelo Original)')
+axs[0].set_xticks(np.arange(len(class_names)))
+axs[0].set_yticks(np.arange(len(class_names)))
+axs[0].set_xticklabels(class_names, rotation=90, fontsize=8)
+axs[0].set_yticklabels(class_names, fontsize=8)
+
+# Plotar a matriz de confusão para o modelo limpo
+axs[1].imshow(confusion_mtx_limpo, interpolation='nearest', cmap=plt.cm.Blues)
+thresh_limpo = confusion_mtx_limpo.max() / 2.0
+for i in range(confusion_mtx_limpo.shape[0]):
+    for j in range(confusion_mtx_limpo.shape[1]):
+        axs[1].text(j, i, confusion_mtx_limpo[i, j],
+                    horizontalalignment="center",
+                    color="white" if confusion_mtx_limpo[i, j] > thresh_limpo else "black",
+                    fontsize=8)
+axs[1].set_title('Matriz de Confusão (Modelo Limpo)')
+axs[1].set_xticks(np.arange(len(class_names)))
+axs[1].set_yticks(np.arange(len(class_names)))
+axs[1].set_xticklabels(class_names, rotation=90, fontsize=8)
+axs[1].set_yticklabels(class_names, fontsize=8)
+
+# Ajustar o espaçamento entre as subplots
+plt.tight_layout()
+
+# Exibir a figura
+plt.show()
+
+
+# Imprimir os indicadores de desempenho para o modelo original
+print("Indicadores de desempenho do modelo original:")
+print("Acurácia:", accuracy_original)
+print("Precisão:", precision_original)
+print("Recall:", recall_original)
+print("F1-score:", f1_score_original)
+
+# Imprimir os indicadores de desempenho para o modelo limpo
+print("\nIndicadores de desempenho do modelo limpo:")
+print("Acurácia:", accuracy_limpo)
+print("Precisão:", precision_limpo)
+print("Recall:", recall_limpo)
+print("F1-score:", f1_score_limpo)
+
